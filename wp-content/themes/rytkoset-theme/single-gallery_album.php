b43cc121ad0d8b9599242f03535b953e0b8b8142
Ilkka Rytkönen <ilkka@ilkkarytkonen.fi>
2026-04-06 10:59:56 +0300

    19 videoiden upotus albumeihin (#130)
    * Add YouTube videos to the albums
    
    * Fix mobile view last thumbnail image size

--- wp-content/themes/rytkoset-theme/single-gallery_album.php
+++ wp-content/themes/rytkoset-theme/single-gallery_album.php
@@ -18,10 +18,44 @@ if ( have_posts() ) :
 		the_post();
 
 		$gallery_images     = function_exists( 'get_field' ) ? (array) get_field( 'gallery_images' ) : array();
-		$gallery_videos     = function_exists( 'get_field' ) ? (array) get_field( 'gallery_videos' ) : array();
+		$gallery_videos     = rytkoset_theme_get_gallery_album_video_rows( get_the_ID() );
 		$album_date         = rytkoset_theme_get_album_display_date( get_the_ID() );
 		$album_description  = trim( (string) get_post_field( 'post_excerpt', get_the_ID() ) );
 		$album_body_content = trim( (string) get_post_field( 'post_content', get_the_ID() ) );
+		$album_videos       = array();
+
+		if ( ! empty( $gallery_videos ) ) {
+			foreach ( $gallery_videos as $video ) {
+				$video_url  = isset( $video['video_url'] ) ? $video['video_url'] : '';
+				$embed_url  = rytkoset_theme_get_video_embed_url( $video_url );
+				$embed_html = $video_url ? wp_oembed_get( $video_url ) : '';
+
+				if ( empty( $embed_url ) && empty( $embed_html ) ) {
+					continue;
+				}
+
+				$thumbnail      = isset( $video['video_thumbnail_id'] ) ? (int) $video['video_thumbnail_id'] : 0;
+				$thumbnail_src  = $thumbnail ? wp_get_attachment_image_url( $thumbnail, 'large' ) : rytkoset_theme_get_video_thumbnail_url( $video_url );
+				$thumbnail_set  = $thumbnail ? wp_get_attachment_image_srcset( $thumbnail, 'large' ) : '';
+				$thumbnail_alt  = $thumbnail ? get_post_meta( $thumbnail, '_wp_attachment_image_alt', true ) : '';
+				$thumbnail_meta = $thumbnail ? wp_get_attachment_image_src( $thumbnail, 'full' ) : array( $thumbnail_src, 480, 360 );
+
+				$album_videos[] = array(
+					'video_url'             => $video_url,
+					'embed_url'             => $embed_url,
+					'embed_html'            => $embed_html,
+					'thumbnail_id'          => $thumbnail,
+					'thumbnail_src'         => $thumbnail_src,
+					'thumbnail_srcset'      => $thumbnail_set,
+					'thumbnail_alt'         => $thumbnail_alt,
+					'thumbnail_width'       => isset( $thumbnail_meta[1] ) ? (int) $thumbnail_meta[1] : 1280,
+					'thumbnail_height'      => isset( $thumbnail_meta[2] ) ? (int) $thumbnail_meta[2] : 720,
+					'caption_html'          => $thumbnail ? rytkoset_theme_get_attachment_caption_html( $thumbnail ) : '',
+					'visible_caption_html'  => $thumbnail ? rytkoset_theme_get_attachment_visible_caption_html( $thumbnail ) : '',
+					'item_id'               => 'video-' . substr( md5( $embed_url ), 0, 12 ),
+				);
+			}
+		}
 		?>
 		<article <?php post_class( 'album' ); ?>>
 			<header class="album-hero<?php echo has_post_thumbnail() ? '' : ' album-hero--no-image'; ?>">
@@ -62,27 +96,18 @@ if ( have_posts() ) :
 							<div class="album__content"><?php the_content(); ?></div>
 						<?php endif; ?>
 
-						<?php if ( ! empty( $gallery_videos ) ) : ?>
+						<?php if ( ! empty( $album_videos ) ) : ?>
 							<div class="album__videos">
 								<h2 class="album__section-title"><?php esc_html_e( 'Videot', 'rytkoset-theme' ); ?></h2>
-								<?php foreach ( $gallery_videos as $video ) : ?>
-									<?php
-									$video_url  = isset( $video['video_url'] ) ? $video['video_url'] : '';
-									$embed_url  = rytkoset_theme_get_video_embed_url( $video_url );
-									$embed_html = $video_url ? wp_oembed_get( $video_url ) : '';
-
-									if ( empty( $embed_url ) && empty( $embed_html ) ) {
-										continue;
-									}
-									?>
+								<?php foreach ( $album_videos as $video ) : ?>
 									<div class="album__video">
 										<div class="album__video-embed">
 											<?php
-											if ( ! empty( $embed_html ) ) {
-												echo $embed_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+											if ( ! empty( $video['embed_html'] ) ) {
+												echo $video['embed_html']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 											} else {
 												?>
-												<iframe src="<?php echo esc_url( $embed_url ); ?>" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen loading="lazy"></iframe>
+												<iframe src="<?php echo esc_url( $video['embed_url'] ); ?>" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen loading="lazy"></iframe>
 												<?php
 											}
 											?>
@@ -127,101 +152,33 @@ if ( have_posts() ) :
 							}
 						}
 
-						if ( ! empty( $gallery_videos ) ) {
-							foreach ( $gallery_videos as $video ) {
-								$video_url = isset( $video['video_url'] ) ? $video['video_url'] : '';
-								$embed_url = rytkoset_theme_get_video_embed_url( $video_url );
-
-								if ( empty( $embed_url ) ) {
-									continue;
-								}
-
-								$thumbnail      = isset( $video['video_thumbnail']['ID'] ) ? (int) $video['video_thumbnail']['ID'] : 0;
-								$thumbnail_src  = $thumbnail ? wp_get_attachment_image_url( $thumbnail, 'large' ) : '';
-								$thumbnail_set  = $thumbnail ? wp_get_attachment_image_srcset( $thumbnail, 'large' ) : '';
-								$thumbnail_alt  = $thumbnail ? get_post_meta( $thumbnail, '_wp_attachment_image_alt', true ) : '';
-								$thumbnail_meta = $thumbnail ? wp_get_attachment_image_src( $thumbnail, 'full' ) : array( 1280, 720 );
-
-								$gallery_items[] = array(
-									'type'                 => 'video',
-									'item_id'              => 'video-' . substr( md5( $embed_url ), 0, 12 ),
-									'video_src'            => $embed_url,
-									'poster'               => $thumbnail_src,
-									'srcset'               => $thumbnail_set,
-									'alt'                  => $thumbnail_alt,
-									'width'                => isset( $thumbnail_meta[1] ) ? (int) $thumbnail_meta[1] : 1280,
-									'height'               => isset( $thumbnail_meta[2] ) ? (int) $thumbnail_meta[2] : 720,
-									'pswp_srcset'          => $thumbnail ? wp_get_attachment_image_srcset( $thumbnail, 'full' ) : '',
-									'pswp_sizes'           => '100vw',
-									'thumb_src'            => $thumbnail_src,
-									'thumb_srcset'         => $thumbnail_set,
-									'caption_html'         => $thumbnail ? rytkoset_theme_get_attachment_caption_html( $thumbnail ) : '',
-									'visible_caption_html' => $thumbnail ? rytkoset_theme_get_attachment_visible_caption_html( $thumbnail ) : '',
-								);
-							}
-						}
-
 						if ( ! empty( $gallery_items ) ) :
 							?>
 							<div class="album__gallery">
-								<h2 class="album__section-title"><?php esc_html_e( 'Kuvat ja videot', 'rytkoset-theme' ); ?></h2>
+								<h2 class="album__section-title"><?php esc_html_e( 'Kuvat', 'rytkoset-theme' ); ?></h2>
 								<div class="gallery-grid js-gallery-grid" data-pswp-gallery="album-<?php echo esc_attr( get_the_ID() ); ?>">
 									<?php foreach ( $gallery_items as $item ) : ?>
-										<?php if ( 'video' === $item['type'] ) : ?>
-											<a
-												class="gallery-grid__item js-gallery-item"
-												href="<?php echo esc_url( $item['video_src'] ); ?>"
-												data-video-src="<?php echo esc_url( $item['video_src'] ); ?>"
-												data-pswp-item-id="<?php echo esc_attr( $item['item_id'] ); ?>"
-												data-pswp-width="<?php echo esc_attr( $item['width'] ); ?>"
-												data-pswp-height="<?php echo esc_attr( $item['height'] ); ?>"
-												<?php if ( ! empty( $item['poster'] ) ) : ?>data-poster="<?php echo esc_url( $item['poster'] ); ?>"<?php endif; ?>
-												<?php if ( ! empty( $item['pswp_srcset'] ) ) : ?>data-pswp-srcset="<?php echo esc_attr( $item['pswp_srcset'] ); ?>"<?php endif; ?>
-												<?php if ( ! empty( $item['pswp_sizes'] ) ) : ?>data-pswp-sizes="<?php echo esc_attr( $item['pswp_sizes'] ); ?>"<?php endif; ?>
-												<?php if ( ! empty( $item['caption_html'] ) ) : ?>data-pswp-caption-html="<?php echo esc_attr( $item['caption_html'] ); ?>"<?php endif; ?>
-											>
-												<?php if ( ! empty( $item['thumb_src'] ) ) : ?>
-													<img
-														class="gallery-grid__image"
-														src="<?php echo esc_url( $item['thumb_src'] ); ?>"
-														<?php if ( ! empty( $item['thumb_srcset'] ) ) : ?>srcset="<?php echo esc_attr( $item['thumb_srcset'] ); ?>"<?php endif; ?>
-														sizes="(min-width: 960px) 25vw, 90vw"
-														alt="<?php echo esc_attr( $item['alt'] ); ?>"
-													/>
-												<?php else : ?>
-													<span class="gallery-grid__placeholder">Video</span>
-												<?php endif; ?>
-												<?php if ( ! empty( $item['visible_caption_html'] ) ) : ?>
-													<div class="gallery-grid__caption"><?php echo wp_kses_post( $item['visible_caption_html'] ); ?></div>
-												<?php endif; ?>
-												<span class="gallery-grid__badge">
-													<span aria-hidden="true">▶</span>
-													<span><?php esc_html_e( 'Video', 'rytkoset-theme' ); ?></span>
-												</span>
-											</a>
-										<?php else : ?>
-											<a
-												class="gallery-grid__item js-gallery-item"
-												href="<?php echo esc_url( $item['src'] ); ?>"
-												data-pswp-item-id="<?php echo esc_attr( $item['item_id'] ); ?>"
-												data-pswp-width="<?php echo esc_attr( $item['width'] ); ?>"
-												data-pswp-height="<?php echo esc_attr( $item['height'] ); ?>"
-												<?php if ( ! empty( $item['pswp_srcset'] ) ) : ?>data-pswp-srcset="<?php echo esc_attr( $item['pswp_srcset'] ); ?>"<?php endif; ?>
-												<?php if ( ! empty( $item['pswp_sizes'] ) ) : ?>data-pswp-sizes="<?php echo esc_attr( $item['pswp_sizes'] ); ?>"<?php endif; ?>
-												<?php if ( ! empty( $item['caption_html'] ) ) : ?>data-pswp-caption-html="<?php echo esc_attr( $item['caption_html'] ); ?>"<?php endif; ?>
-											>
-												<img
-													class="gallery-grid__image"
-													src="<?php echo esc_url( $item['thumb_src'] ); ?>"
-													<?php if ( ! empty( $item['thumb_srcset'] ) ) : ?>srcset="<?php echo esc_attr( $item['thumb_srcset'] ); ?>"<?php endif; ?>
-													sizes="<?php echo esc_attr( $item['sizes'] ); ?>"
-													alt="<?php echo esc_attr( $item['alt'] ); ?>"
-												/>
-												<?php if ( ! empty( $item['visible_caption_html'] ) ) : ?>
-													<div class="gallery-grid__caption"><?php echo wp_kses_post( $item['visible_caption_html'] ); ?></div>
-												<?php endif; ?>
-											</a>
-										<?php endif; ?>
+										<a
+											class="gallery-grid__item js-gallery-item"
+											href="<?php echo esc_url( $item['src'] ); ?>"
+											data-pswp-item-id="<?php echo esc_attr( $item['item_id'] ); ?>"
+											data-pswp-width="<?php echo esc_attr( $item['width'] ); ?>"
+											data-pswp-height="<?php echo esc_attr( $item['height'] ); ?>"
+											<?php if ( ! empty( $item['pswp_srcset'] ) ) : ?>data-pswp-srcset="<?php echo esc_attr( $item['pswp_srcset'] ); ?>"<?php endif; ?>
+											<?php if ( ! empty( $item['pswp_sizes'] ) ) : ?>data-pswp-sizes="<?php echo esc_attr( $item['pswp_sizes'] ); ?>"<?php endif; ?>
+											<?php if ( ! empty( $item['caption_html'] ) ) : ?>data-pswp-caption-html="<?php echo esc_attr( $item['caption_html'] ); ?>"<?php endif; ?>
+										>
+											<img
+												class="gallery-grid__image"
+												src="<?php echo esc_url( $item['thumb_src'] ); ?>"
+												<?php if ( ! empty( $item['thumb_srcset'] ) ) : ?>srcset="<?php echo esc_attr( $item['thumb_srcset'] ); ?>"<?php endif; ?>
+												sizes="<?php echo esc_attr( $item['sizes'] ); ?>"
+												alt="<?php echo esc_attr( $item['alt'] ); ?>"
+											/>
+											<?php if ( ! empty( $item['visible_caption_html'] ) ) : ?>
+												<div class="gallery-grid__caption"><?php echo wp_kses_post( $item['visible_caption_html'] ); ?></div>
+											<?php endif; ?>
+										</a>
 									<?php endforeach; ?>
 								</div>
 							</div>
